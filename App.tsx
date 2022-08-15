5d7c2b2149beb47750cabb017ef088f741681060
Antonin Urban <antonin.urban@applifting.cz>
2022-08-15 19:03:47 +0200

    chore: add polyfills for RxDB

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 // for this first import see https://github.com/uuidjs/uuid/issues/416
 import 'react-native-get-random-values';
+import { decode, encode } from 'base-64';
 import { useState, useEffect } from 'react';
 import { AppContext } from './src/appContext';
 import { initializeDb } from './src/db/initializeDb';
 import Heroes from './Hereos';
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
+
+// Avoid using node dependent modules
+process.browser = true;
 
 export default function App() {
   const [db, setDb] = useState(null);
